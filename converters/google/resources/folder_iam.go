15fabec51b7d21379e6d8e502bc4c6ab60af0079
Modular Magician <magic-modules@google.com>
2022-06-16 16:17:25 +0000

    Old generated code for MM PR 6127.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- converters/google/resources/folder_iam.go
+++ converters/google/resources/folder_iam.go
@@ -90,7 +90,7 @@ func newFolderIamAsset(
 
 func FetchFolderIamPolicy(d TerraformResourceData, config *Config) (Asset, error) {
 	return fetchIamPolicy(
-		NewFolderIamUpdater,
+		NewProjectIamUpdater,
 		d,
 		config,
 		"//cloudresourcemanager.googleapis.com/{{folder}}",

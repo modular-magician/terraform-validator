4fc76d1d39eaf9fa1664f3395e59f64a9465fbc8
Modular Magician <magic-modules@google.com>
2022-07-08 18:39:36 +0000

    New generated code for MM PR 6241.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- converters/google/resources/config.go
+++ converters/google/resources/config.go
@@ -201,6 +201,7 @@ type Config struct {
 	GameServicesBasePath         string
 	GKEHubBasePath               string
 	HealthcareBasePath           string
+	IAMBetaBasePath              string
 	IapBasePath                  string
 	IdentityPlatformBasePath     string
 	KMSBasePath                  string
@@ -292,6 +293,7 @@ const FirestoreBasePathKey = "Firestore"
 const GameServicesBasePathKey = "GameServices"
 const GKEHubBasePathKey = "GKEHub"
 const HealthcareBasePathKey = "Healthcare"
+const IAMBetaBasePathKey = "IAMBeta"
 const IapBasePathKey = "Iap"
 const IdentityPlatformBasePathKey = "IdentityPlatform"
 const KMSBasePathKey = "KMS"
@@ -377,6 +379,7 @@ var DefaultBasePaths = map[string]string{
 	GameServicesBasePathKey:         "https://gameservices.googleapis.com/v1/",
 	GKEHubBasePathKey:               "https://gkehub.googleapis.com/v1/",
 	HealthcareBasePathKey:           "https://healthcare.googleapis.com/v1/",
+	IAMBetaBasePathKey:              "https://iam.googleapis.com/v/",
 	IapBasePathKey:                  "https://iap.googleapis.com/v1/",
 	IdentityPlatformBasePathKey:     "https://identitytoolkit.googleapis.com/v2/",
 	KMSBasePathKey:                  "https://cloudkms.googleapis.com/v1/",
@@ -1224,6 +1227,7 @@ func ConfigureBasePaths(c *Config) {
 	c.GameServicesBasePath = DefaultBasePaths[GameServicesBasePathKey]
 	c.GKEHubBasePath = DefaultBasePaths[GKEHubBasePathKey]
 	c.HealthcareBasePath = DefaultBasePaths[HealthcareBasePathKey]
+	c.IAMBetaBasePath = DefaultBasePaths[IAMBetaBasePathKey]
 	c.IapBasePath = DefaultBasePaths[IapBasePathKey]
 	c.IdentityPlatformBasePath = DefaultBasePaths[IdentityPlatformBasePathKey]
 	c.KMSBasePath = DefaultBasePaths[KMSBasePathKey]

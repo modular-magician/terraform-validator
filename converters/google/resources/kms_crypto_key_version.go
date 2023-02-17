40b294f1171b7cb5dc1607e34b09df2c856aaa1b
Modular Magician <magic-modules@google.com>
2023-02-17 20:25:37 +0000

    New generated code for MM PR 7245.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- converters/google/resources/kms_crypto_key_version.go
+++ converters/google/resources/kms_crypto_key_version.go
@@ -54,10 +54,50 @@ func GetKMSCryptoKeyVersionApiObject(d TerraformResourceData, config *Config) (m
 	} else if v, ok := d.GetOkExists("state"); !isEmptyValue(reflect.ValueOf(stateProp)) && (ok || !reflect.DeepEqual(v, stateProp)) {
 		obj["state"] = stateProp
 	}
+	externalProtectionLevelOptionsProp, err := expandKMSCryptoKeyVersionExternalProtectionLevelOptions(d.Get("external_protection_level_options"), d, config)
+	if err != nil {
+		return nil, err
+	} else if v, ok := d.GetOkExists("external_protection_level_options"); !isEmptyValue(reflect.ValueOf(externalProtectionLevelOptionsProp)) && (ok || !reflect.DeepEqual(v, externalProtectionLevelOptionsProp)) {
+		obj["externalProtectionLevelOptions"] = externalProtectionLevelOptionsProp
+	}
 
 	return obj, nil
 }
 
 func expandKMSCryptoKeyVersionState(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
+	return v, nil
+}
+
+func expandKMSCryptoKeyVersionExternalProtectionLevelOptions(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+	raw := l[0]
+	original := raw.(map[string]interface{})
+	transformed := make(map[string]interface{})
+
+	transformedExternalKeyUri, err := expandKMSCryptoKeyVersionExternalProtectionLevelOptionsExternalKeyUri(original["external_key_uri"], d, config)
+	if err != nil {
+		return nil, err
+	} else if val := reflect.ValueOf(transformedExternalKeyUri); val.IsValid() && !isEmptyValue(val) {
+		transformed["externalKeyUri"] = transformedExternalKeyUri
+	}
+
+	transformedEkmConnectionKeyPath, err := expandKMSCryptoKeyVersionExternalProtectionLevelOptionsEkmConnectionKeyPath(original["ekm_connection_key_path"], d, config)
+	if err != nil {
+		return nil, err
+	} else if val := reflect.ValueOf(transformedEkmConnectionKeyPath); val.IsValid() && !isEmptyValue(val) {
+		transformed["ekmConnectionKeyPath"] = transformedEkmConnectionKeyPath
+	}
+
+	return transformed, nil
+}
+
+func expandKMSCryptoKeyVersionExternalProtectionLevelOptionsExternalKeyUri(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
+	return v, nil
+}
+
+func expandKMSCryptoKeyVersionExternalProtectionLevelOptionsEkmConnectionKeyPath(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
 	return v, nil
 }
